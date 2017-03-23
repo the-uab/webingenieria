1cbc9d1b28a3c02f645ce6baf69fab0fa62386c1
Everson Vidal <flextoo@hotmail.com>
2017-03-23 04:54:53 -0400

    vidal
    cambios vidal

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Vidal Login</title>
+  
+   
+      <link rel="stylesheet" href="css/style.css">
+  
+</head>
+
+<body>
+  <div class="box">
+  <div id="header">
+  <h1 id="logintoregister">Login<img src="images/logoing.jpg" width="100" height="100" align="right" hspace="50"></h1>
+  </div> 
+   <form action="" method="post">
+    <div class="group">      
+      <input class="inputMaterial" type="text" required>
+      <span class="highlight"></span>
+      <span class="bar"></span>
+      <label>Nombre de Usuario</label>
+    </div>
+	    <div class="group">      
+      <input class="inputMaterial" type="password" required>
+      <span class="highlight"></span>
+      <span class="bar"></span>
+      <label>Password</label>
+    </div>
+    <button id="buttonlogintoregister" type="submit">Ingresar..</button>
+  </form>
+  <div id="footer-box"><p class="footer-text">No esta registrado..?<span class="sign-up"> Registrarse</span></p></div>
+</div>
+   
+</body>
+</html>
