a8b45882fd986ac7ebf505a1d92943ac478c7328
Mario Ch <mariouab@gmail.com>
2017-03-30 10:04:28 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title> Pagina principal </title>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="jumbotron text-center">
+        <h1>Facultad de Ingenieria</h1>
+        <img  class="img-rounded" src="images/logoing.jpg" width="100" height="100"> 
+        <p>Tu Presente y tu futuro</p> 
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+            <h3>Ingenieria de Sistemas</h3>
+            <p>Lorem ipsum dolor..</p>
+            <p>Ut enim ad..</p>
+            </div>
+            <div class="col-sm-4">
+            <h3>Ingenieria en Redes y Telecomunicaciones</h3>
+            <p>Lorem ipsum dolor..</p>
+            <p>Ut enim ad..</p>
+            </div>
+            <div class="col-sm-4">
+            <h3>Ingenieria Ambiental</h3> 
+            <p>Lorem ipsum dolor..</p>
+            <p>Ut enim ad..</p>
+            </div>
+        </div>
+        </div>  
+    </body>
+</html>
