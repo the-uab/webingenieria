7d7bc64b015ed8042f90060ee661cc12a0e2cb10
Elias Sauer <jels1991@gmail.com>
2017-03-30 09:04:51 -0400

    Mision-Vision-Historia

---
+++ mision.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Historia - Misión - Visión</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+<div id="skipnav"><a href="#maincontent">Skip to main content</a></div>
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="#page-top">Historia - Misión - Visión</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="login.html">Login</a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Header -->
+    <header>
+        
+    </header>
+
+    <!-- Portfolio Grid Section -->
+    
+
+    <!-- About Section -->
+    <section class="success" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Historia</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>El 30 de enero del 2013, mediante Resolución Ministerial N° 048/2013, se autoriza la apertura la Carrera de Ingeniería en Redes y Telecomunicaciones con 9El avance y la exigencia del desarrollo de la ciencia; en los campos de informática, Telemática, Inteligencia artificial, etc. Han motivado que la Universidad aperture y cree la carrera de Ingeniería de Sistemas a nivel de licenciatura, cuyo funcionamiento fue autorizado por la Resolución Secretarial Nº 336 el 4 de agosto de 1997 en la ciudad de la Paz y conforme al Art. 188 de la Constitución Política del Estado y a lo dispuesto por el Reglamento Institucional de Universidades Privadas.</p>
+                </div>
+                <div class="col-lg-4">
+                    <p> Aprobado por Resolución Ministerial Nº 020/97 el 14 de febrero de 1997. La creación de la carrera de Ingeniería de Sistemas, se origina por la inquietud de la administración de la UAB y de la Patrocinadora de la UAB la Unión Boliviana de la Iglesia Adventista del Séptimo Día. Es así que en una reunión de la Patrocinadora se acuerda nombrar una comisión de elaboración del proyecto para la creación de esta carrera. La Facultad de Ingeniería, inició sus actividades académicas en agosto de 1997.</p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Misión</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>Extender la experiencia educacional, al desarrollo del conocimiento de la tecnología de información y la adquisición de habilidades, para proveer a la sociedad individuos líderes con desarrollo integral de las facultades físicas, mentales, </p>
+                </div>
+                <div class="col-lg-4">
+                    <p>sociales y espirituales preparadas para alcanzar su máximo potencial, mediante una vida de servicio, caracterizado por la excelencia académica sin distinción social, género, étnicos, políticos o religiosos, con altos principios éticos y morales.</p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Visión</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>Las Carreras del Área de Sistemas de la Facultad de Ingeniería están plenamente identificadas con la Visión Institucional de la Universidad Adventista de Bolivia; educar para esta vida y la eternidad con desarrollo armonioso de las potencialidades físicas, mentales,</p>
+                </div>
+                <div class="col-lg-4">
+                    <p> sociales y espirituales para una vida de servicio; creando un ambiente universitario de liderazgo innovador, proyectando los valores del evangelio como testimonio cristiano, personal y grupal de los integrantes de la Facultad de Ingeniería.</p>
+                </div>
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        By: Jonathan Elias Leonardi Sauer
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+ 
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/freelancer.min.js"></script>
+
+</body>
+
+</html>
