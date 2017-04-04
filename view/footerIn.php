bc0dc5b7b2c9f3792e0a0ad34c8dcd7a5f860707
Mario Ch <mariouab@gmail.com>
2017-04-04 10:56:23 -0400

    cambio de pie de pagina

--- view/footerIn.php
+++ view/footerIn.php
@@ -1,16 +1,29 @@
-<footer class="page-footer teal">
-  <div class="footer-copyright">
-    <div class="container">
-    Made by <a class="brown-text text-lighten-3" href="#">LEGIN TB -> UAB</a>
-    </div>
-  </div>
-</footer>
-
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Facultad de Ingeniería</h5>
+                <p class="grey-text text-lighten-4">Tu presente y tu futuro.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Enlaces</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Youtube</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twiter</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright DidSoft
+            <a class="grey-text text-lighten-4 right" href="#!">UAB</a>
+            </div>
+          </div>
+        </footer>
 
 <!--  Scripts-->
 <script src="js/jquery.min.js"></script>
 <script src="js/materialize.min.js"></script>
 <script src="js/init.js"></script>
-
-</body>
-</html>
