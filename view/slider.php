c3eca1c94ec74f8149dd347fc41579a59d6b21fa
Elias Sauer <jels1991@gmail.com>
2017-04-04 11:59:54 -0400

    Creacion de un slider

---
+++ view/slider.php
@@ -0,0 +1,22 @@
+<div class="container">
+  <div class="row">
+    <div class="col s12">
+        <div class="slider">
+            <ul class="slider">
+                <li> 
+                    <img src="multimedia/img/uab.jpg"> 
+                </li>
+                <li> 
+                    <img src="multimedia/img/ambiental.jpg"> 
+                </li>
+                <li> 
+                    <img src="multimedia/img/telecom.jpg"> 
+                </li>
+                <li> 
+                    <img src="multimedia/img/sistem.jpg"> 
+                </li>
+            </ul>
+            
+        </div>
+    </div>
+</div>
