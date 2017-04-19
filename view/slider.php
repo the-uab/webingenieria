73d827e530387d5495a61de6ea6a7c3bcba216dd
Elias Sauer <jels1991@gmail.com>
2017-04-19 16:22:55 -0400

    Mejoras en los Sliders

--- view/slider.php
+++ view/slider.php
@@ -5,16 +5,31 @@
         <div class="slider">
             <ul class="slides">
                 <li>
-                    <img src="multimedia/img/uab.jpg" class="responsive-img">
+                    <img src="multimedia/img/sistemas-4.jpg" class="responsive-img">
                 </li>
                 <li>
-                    <img src="multimedia/img/ambiental.jpg" class="responsive-img">
+                    <img src="multimedia/img/redes-1.jpg" class="responsive-img">
                 </li>
                 <li>
-                    <img src="multimedia/img/telecom.jpg" class="responsive-img">
+                    <img src="multimedia/img/ambiental-1.jpg" class="responsive-img">
                 </li>
                 <li>
-                    <img src="multimedia/img/sistem-5.jpg" class="responsive-img">
+                    <img src="multimedia/img/sistemas-2.jpg" class="responsive-img">
+                </li>
+                <li>
+                    <img src="multimedia/img/redes-6.jpg" class="responsive-img">
+                </li>
+                <li>
+                    <img src="multimedia/img/ambiental-4.jpg" class="responsive-img">
+                </li>
+                <li>
+                    <img src="multimedia/img/sistemas-3.jpg" class="responsive-img">
+                </li>
+                <li>
+                    <img src="multimedia/img/redes-3.jpg" class="responsive-img">
+                </li>
+                <li>
+                    <img src="multimedia/img/ambiental-3.png" class="responsive-img">
                 </li>
             </ul>
 
